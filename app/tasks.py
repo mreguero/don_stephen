b5ff8d67c8a35a20b6870f94231af44dc2f137b7
Miguel Reguero <mreguerob0@gmail.com>
2015-11-29 13:59:14 +0000

    multiplatform path support

--- app/tasks.py
+++ app/tasks.py
@@ -4,6 +4,8 @@ from django.core.files.base import ContentFile
 from .models import Feature
 from behave.runner_util import make_undefined_step_snippet
 
+BASEPATH = os.path.dirname(__file__)
+
 def gen_feature_file(feature_id):
 
     feature = Feature.objects.get(id=feature_id)
@@ -25,15 +27,17 @@ def gen_feature_file(feature_id):
 
 def make_test_funcs(steps, feature_id):
 
-    with open('stephen_demo_repo\\test_{}.py'.format(feature_id), 'w') as f:
+    filename = 'test_{}.py'.format(feature_id)
+    with open(os.path.join('stephen_demo_repo', filename), 'w') as f:
         for step in steps:
             test_fun = make_undefined_step_snippet(step)
             f.write(test_fun)
-    return "test_{}.py".format(feature_id)
+    return filename
 
 def add_to_repo(filename, feature):
-    os.chdir(r'C:\Users\miguel\Documents\GitHub\don_stephen\stephen_demo_repo')
+    project_path = os.path.join(BASEPATH, '..')
+    os.chdir(r'{}'.format(os.path.join(project_path, 'stephen_demo_repo')))
     os.system('git add {}'.format(filename))
-    os.system('git commit -m "Added tests for feature {}'.format(feature))
+    os.system('git commit -m "Added tests for feature {}"'.format(feature))
     os.system('git push origin master')
-    os.chdir(r"C:\Users\miguel\Documents\GitHub\don_stephen")
+    os.chdir(r"{}".format(project_path))
